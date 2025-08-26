d4e8e6b7b8fd3e004b16e8e9518f6c4a073fbc37
the-unknown-code <hello@unk.digital>
2025-08-26 23:33:49 +0700

    update

--- app.config.ts
+++ app.config.ts
@@ -1,3 +1,6 @@
 export default {
 	ssr: false,
+	link: {
+		prefetch: true,
+	},
 };
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,8 @@
 export default defineNuxtConfig({
 	modules: ['@nuxt/eslint', '@pinia/nuxt', '@vueuse/nuxt'],
 	devtools: { enabled: true },
+	css: ['@/assets/main.scss'],
 	compatibilityDate: '2025-07-15',
-
 	eslint: {
 		checker: true,
 	},
